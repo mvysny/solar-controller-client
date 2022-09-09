260d13d84fb0a3e6a8132e7dda4640d54218bf4d
Martin Vysny <mavi@vaadin.com>
2022-09-09 08:40:33 +0300

    minor

--- src/nativeTest/kotlin/RenogyModbusClientTest.kt
+++ src/nativeTest/kotlin/RenogyModbusClientTest.kt
@@ -1,5 +1,4 @@
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.expect
 import kotlin.test.fail
 
--- src/nativeTest/kotlin/crc/CRC16ModbusTest.kt
+++ src/nativeTest/kotlin/crc/CRC16ModbusTest.kt
@@ -1,7 +1,6 @@
 package crc
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.expect
 
 class CRC16ModbusTest {
