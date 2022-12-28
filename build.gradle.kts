86ef6551a03eca3fcb37805f3cf1e6c098ac7dff
Martin Vysny <mavi@vaadin.com>
2022-12-28 10:49:45 +0200

    kotlin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
-    kotlin("multiplatform") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("multiplatform") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 }
 
 group = "me.mavi"
