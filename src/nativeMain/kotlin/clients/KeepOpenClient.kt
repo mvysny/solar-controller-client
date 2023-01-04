ff67d52c88d6fba8bd96758cda400e89a0f2199c
Martin Vysny <mavi@vaadin.com>
2023-01-04 16:03:14 +0200

    minor

--- src/nativeMain/kotlin/clients/KeepOpenClient.kt
+++ src/nativeMain/kotlin/clients/KeepOpenClient.kt
@@ -3,43 +3,6 @@ package clients
 import utils.*
 
 /**
- * Opens a comm pipe on every request, then closes it afterwards.
- */
-class OpenCloseClient(val file: File) : RenogyClient {
-    private fun open(): SerialPort {
-        val io = SerialPort(file)
-        io.configure()
-        return io
-    }
-
-    private fun <T> withSerialPort(block: (SerialPort) -> T): T =
-        open().use { serialPort ->
-            try {
-                block(serialPort)
-            } catch (e: RenogyException) {
-                // perhaps there's some leftover data in the serial port? Drain.
-                log.warn("Caught $e, draining $serialPort")
-                serialPort.drainQuietly()
-                throw e
-            }
-        }
-
-    override fun getSystemInfo(): SystemInfo =
-        withSerialPort { RenogyModbusClient(it).getSystemInfo() }
-
-    override fun getAllData(cachedSystemInfo: SystemInfo?): RenogyData =
-        withSerialPort { RenogyModbusClient(it).getAllData(cachedSystemInfo) }
-
-    override fun close() {}
-
-    override fun toString(): String = "OpenCloseClient($file)"
-
-    companion object {
-        private val log = Log.get(OpenCloseClient::class)
-    }
-}
-
-/**
  * Keeps a comm pipe open during the whole duration of this client.
  * Workarounds https://github.com/mvysny/solar-controller-client/issues/10 by closing/reopening
  * the pipe on timeout.
